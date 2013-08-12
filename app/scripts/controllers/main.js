095502963cb29c0afc2dd71ac93ee9273e79466d
guym <guym@gigaspaces.com>
2013-08-12 16:14:10 +0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cloudifyWidgetHpClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
