19406f1f0ca4b6414567dc416475f3bfdfff03f6
guym <guym@gigaspaces.com>
2013-08-15 00:04:31 +0300

    fixing build

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,16 @@
 echo "I am checking out revision $1" > upgrade.output
 
+echo "pulling from git"
 git pull
+
+echo "checking out $1"
 git checkout $1
+
+echo "running npm install"
 npm install
+
+echo "running bower install"
 bower install
-grunt --force
+
+echo "running grunt --force"
+grunt build --force
