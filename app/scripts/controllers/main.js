5b86e34cf4a53e5574e9e5dbb5dd8e6e0fd45eaa
Erez Carmel <erez@gigaspaces.com>
2013-09-24 16:38:49 +0300

    HP feedback changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,25 +5,25 @@ angular.module('cloudifyWidgetHpClientApp')
 
         $('#mainStepSeeDemo').click(function() {
             $scope.$apply(function() {
-                $location.path("/demo");
+                $location.path('/demo');
             });
         });
 
         $('#mainStepRunPreview').click(function() {
             $scope.$apply(function() {
-                $location.path("/preview");
+                $location.path('/preview');
             });
         });
 
         $('#mainStepSignup').click(function() {
             $scope.$apply(function() {
-                $location.path("/signup");
+                $location.path('/signup');
             });
         });
 
         $('#mainStepRegistered').click(function() {
             $scope.$apply(function() {
-                $location.path("/registered");
+                $location.path('/registered');
             });
         });
 
