997f7753e200bb636b4923272eb4321b7ca77832
Erez Carmel <erez@gigaspaces.com>
2013-10-15 13:25:25 +0300

    iPad functionality fix

--- app/vendors/widgetSkin.js
+++ app/vendors/widgetSkin.js
@@ -7,49 +7,51 @@ $(function () {
 
     $(document).bind('DOMSubtreeModified',function(e) {
         if (e.target.id === 'play_btn') {
-            setEventHandling();
+            setPlayEventHandling();
             $(document).unbind('DOMSubtreeModified');
         }
     });
 
-    function setEventHandling() {
+    $('#play_btn').live('click', function() {
+        alert('in live play click');
+        if (!credentialsChecked()) {
+            return;
+        }
 
-        $('#play_btn').on('click', function() {
-            if (!credentialsChecked()) {
-                return;
-            }
+        var iframe = $('#iframe');
+        var postObj = {name: 'play_widget'};
+        if (getAdvanced().project !== '' && getAdvanced().key !== '' && getAdvanced().secretKey !== '') {
+            postObj.advanced = getAdvanced();
+        }
 
-            var iframe = $('#iframe');
-            var postObj = {name: 'play_widget'};
-            if (getAdvanced().project !== '' && getAdvanced().key !== '' && getAdvanced().secretKey !== '') {
-                postObj.advanced = getAdvanced();
-            }
+        updateButtonState('play');
 
-            updateButtonState('play');
+        $('#iframe').trigger({type: "prolong"});
+        $.postMessage(JSON.stringify(postObj), postUrl, iframe.get(0).contentWindow);
+    });
 
-            $('#iframe').trigger({type: "prolong"});
-            $.postMessage(JSON.stringify(postObj), postUrl, iframe.get(0).contentWindow);
-        });
+    $('#stop_btn').live('click', function () {
+        var iframe = $('#iframe');
 
-        $('#stop_btn').on('click', function () {
-            var iframe = $('#iframe');
+        updateButtonState('stop');
 
-            updateButtonState('stop');
+        $.postMessage(JSON.stringify({name: 'stop_widget'}), postUrl, iframe.get(0).contentWindow);
+    });
 
-            $.postMessage(JSON.stringify({name: 'stop_widget'}), postUrl, iframe.get(0).contentWindow);
-        });
+    $('input[name="project_name"], input[name="hpcs_key"], input[name="hpcs_secret_key"]').live('change', function () {
+        if (credentialsChecked()) {
+            $('#play_btn').removeClass('disabled');
+        } else {
+            $('#play_btn').addClass('disabled');
+        }
+    });
 
-        $('input[name="project_name"], input[name="hpcs_key"], input[name="hpcs_secret_key"]').on('change', function () {
-            if (credentialsChecked()) {
-                $('#play_btn').removeClass('disabled');
-            } else {
-                $('#play_btn').addClass('disabled');
-            }
-        });
+    $('#iframe').live('widget_change', function() {
+        updateButtonState('stop');
+    });
 
-        $('#iframe').live('widget_change', function() {
-            updateButtonState('stop');
-        });
+    function setPlayEventHandling() {
+        $('#play_btn').on('click', function() {});
     }
 
     function getAdvanced() {
