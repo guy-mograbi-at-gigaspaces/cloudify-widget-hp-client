aa8bde1045e8f77e8889cfa14c6ac4b448fb4ed1
Erez Carmel <erez@gigaspaces.com>
2013-10-24 18:22:19 +0300

    Moved all widget code to AngularJS

--- app/scripts/controllers/preview.js
+++ app/scripts/controllers/preview.js
@@ -3,16 +3,10 @@
 angular.module('cloudifyWidgetHpClientApp')
     .controller('PreviewCtrl', function ($scope, $location, $timeout, $cookieStore, widgetService) {
 
-        var timeout = 0;
-        var milliseconds = 0;
-        var isNewWidgetSelected = false;
         $scope.currentStep = $location.path() === '/registered' ? 4 : 2;
         $scope.selectedWidget = null;
         $scope.widgetTime = '';
-        $scope.pageUrl = $location.protocol() +'://' + $location.host();
         $scope.conf = window.conf;
-        $scope.manageUrl = null;
-        $scope.widgetLog = [];
 
         widgetService.getWidgetList()
             .then(function(data) {
@@ -21,128 +15,6 @@ angular.module('cloudifyWidgetHpClientApp')
 
         $scope.widgetClick = function (widget) {
             $scope.widgetTime = '';
-            $scope.manageUrl = null;
-            $scope.consoleUrl = null;
-            $scope.widgetLog = [];
             $scope.selectedWidget = widget;
-            isNewWidgetSelected = true;
-            _stopTimer();
-            $('#iframe').trigger({type: 'widget_change'});
         };
-
-        $scope.onTimeout = function() {
-            milliseconds -= 1000;
-            $scope.widgetTime = _millisecondsToTime(milliseconds);
-            timeout = $timeout($scope.onTimeout, 1000);
-        };
-
-        $('#iframe').live('widget_status', function(e) {
-            milliseconds = e.status.timeleftMillis;
-            $cookieStore.put('instanceId', e.status.instanceId);
-
-            if (e.status.publicIp !== null) {
-                $scope.manageUrl = 'http://' + e.status.publicIp + ':8099/';
-            } else {
-                $scope.manageUrl = null;
-            }
-
-            if (e.status.instanceIsAvailable === true) {
-                $scope.consoleUrl = e.status.consoleLink.url;
-            } else {
-                $scope.consoleUrl = null;
-            }
-
-            if (isNewWidgetSelected) {
-                $scope.$apply(function() {
-                    $scope.widgetLog = e.status.output;
-                    isNewWidgetSelected = false;
-                });
-            }
-
-            _startTimer();
-        });
-
-        $('#iframe').live('stop_widget', function() {
-            $scope.widgetTime = '';
-            _stopTimer();
-        });
-
-        $('#iframe').live('prolong', function() {
-            var data = {
-                'leadId' : $cookieStore.get('leadId'),
-                'instanceId' : $cookieStore.get('instanceId')
-            };
-
-            if (data.leadId !== undefined && data.instanceId !== undefined) {
-                widgetService.prolong(data);
-            }
-        });
-
-        $('#iframe').live('error', function() {
-            var data = {};
-
-            if ($cookieStore.get('leadId') !== undefined) {
-                data.leadId = $cookieStore.get('leadId');
-            }
-
-            if ($cookieStore.get('instanceId') !== undefined) {
-                data.instanceId = $cookieStore.get('instanceId');
-            }
-
-            if ($cookieStore.get('leadMail') !== undefined) {
-                data.leadMail = $cookieStore.get('leadMail');
-            }
-
-            if (mixpanel.get_distinct_id() !== undefined) {
-                mixpanel.identify(data.leadMail);
-                mixpanel.people.identify(data.leadMail);
-                mixpanel.track('HP Widget error', data);
-            }
-
-            widgetService.reportError(data);
-        });
-
-        $('#iframe').live('widget_log', function(log) {
-            if (isNewWidgetSelected) {
-                return;
-            }
-            $scope.$apply(function() {
-                if (log.html.charAt(0) === '.') {
-                    $scope.widgetLog.pop();
-                    $scope.widgetLog.push(log.html);
-                } else {
-                    $scope.widgetLog.splice($scope.widgetLog.length - 1, 0, log.html);
-                }
-            });
-        });
-
-        function _startTimer() {
-            _stopTimer();
-            timeout = $timeout($scope.onTimeout, 1000);
-        }
-
-        function _stopTimer() {
-            $timeout.cancel(timeout);
-        }
-
-        function _millisecondsToTime(milli)
-        {
-            var seconds = Math.floor((milli / 1000) % 60);
-            var minutes = Math.floor((milli / (60 * 1000)) % 60);
-            var days = Math.floor(milli / (1000 * 60 * 60 * 24));
-            var timeToDisplay = '';
-
-            if (seconds < 0 || minutes < 0) {
-                seconds = '--';
-                minutes = '--';
-            }
-
-            if (days > 0) {
-                timeToDisplay = days + ' Days';
-            } else {
-                timeToDisplay = minutes + ':' + (seconds < 10 ? '0' : '') + seconds + ' Min.';
-            }
-
-            return timeToDisplay;
-        }
     });
--- app/scripts/directives/widget.js
+++ app/scripts/directives/widget.js
@@ -6,162 +6,238 @@ angular.module('cloudifyWidgetHpClientApp')
             templateUrl: '/views/widgetSkin.html',
             restrict: 'A',
             scope: {
-
+                selectedWidget: '=',
+                currentStep: '=',
+                widgetTime: '='
             },
-            controller:function( $scope ){
+            controller:function($scope, $element, $location, $cookieStore, $timeout, widgetService){
 
+                $scope.postUrl = 'http://' + window.conf.widgetServer;
+                $scope.pageUrl = $location.protocol() +'://' + $location.host();
                 $scope.play = false;
-                $scope.widgetTime = "guy";
+                $scope.playEnabled = $scope.currentStep === 2 && $scope.selectedWidget !== null;
+                $scope.advanced = {
+                    project_name: '',
+                    hpcs_key: '',
+                    hpcs_secret_key: ''
+                };
+                $scope.manageUrl = null;
+                $scope.consoleUrl = null;
+                $scope.widgetLog = [];
 
-                var postUrl = "http://" + conf.widgetServer;
-//                var currentView = this.location.hash.substr(2);
+                var currentView = $location.url().substr(1);
+                var timeout = 0;
+                var milliseconds = 0;
+                var isNewWidgetSelected = false;
+                var handlers = {
+                    'widget_log': function(e) {
+                        if (isNewWidgetSelected) {
+                            return;
+                        }
+
+                        $scope.$apply(function() {
+                            var msg = JSON.parse(e.data);
+
+                            if (msg.message.charAt(0) === '.') {
+                                $scope.widgetLog.pop();
+                                $scope.widgetLog.push(msg.message);
+                            } else {
+                                $scope.widgetLog.splice($scope.widgetLog.length - 1, 0, msg.message);
+                            }
+                            _scrollLog();
+
+                            if (msg.type === 'error') {
+                                var data = {};
+
+                                if ($cookieStore.get('leadId') !== undefined) {
+                                    data.leadId = $cookieStore.get('leadId');
+                                }
+
+                                if ($cookieStore.get('instanceId') !== undefined) {
+                                    data.instanceId = $cookieStore.get('instanceId');
+                                }
+
+                                if ($cookieStore.get('leadMail') !== undefined) {
+                                    data.leadMail = $cookieStore.get('leadMail');
+                                }
+
+                                if (mixpanel.get_distinct_id() !== undefined) {
+                                    mixpanel.identify(data.leadMail);
+                                    mixpanel.people.identify(data.leadMail);
+                                    mixpanel.track('HP Widget error', data);
+                                }
+
+                                widgetService.reportError(data);
+                            }
+                        });
+                    },
+                    'set_advanced': function(e) {
+                        var msg = JSON.parse(e.data);
+                        $scope.advanced.project_name = msg.project;
+                        $scope.advanced.hpcs_key = msg.key;
+                        $scope.advanced.hpcs_secret_key = msg.secretKey;
+                    },
+                    'widget_status': function(e) {
+                        var msg = JSON.parse(e.data);
+                        milliseconds = msg.status.timeleftMillis;
+                        $cookieStore.put('instanceId', msg.status.instanceId);
+
+                        if (msg.status.publicIp !== null) {
+                            $scope.manageUrl = 'http://' + msg.status.publicIp + ':8099/';
+                        } else {
+                            $scope.manageUrl = null;
+                        }
+
+                        if (msg.status.instanceIsAvailable === true) {
+                            $scope.consoleUrl = msg.status.consoleLink.url;
+                        } else {
+                            $scope.consoleUrl = null;
+                        }
+
+                        if (isNewWidgetSelected) {
+                            $scope.$apply(function() {
+                                $scope.widgetLog = msg.status.output;
+                                isNewWidgetSelected = false;
+                            });
+                        }
+
+                        $scope.play = msg.status.state === 'STOPPED' ? false : true;
+
+                        _startTimer();
+                    },
+                    'stop_widget': function() {
+                        $scope.widgetTime = '';
+                        _stopTimer();
+                    }
+                };
 
                 $scope.playWidget = function(){
-                    console.log("play!");
+                    if (!$scope.credentialsChecked()) {
+                        return;
+                    }
+
                     $scope.play = true;
+                    var iframe = $element.find('#iframe');
+                    var postObj = {name: 'play_widget'};
+                    if (_getAdvanced().project !== '' && _getAdvanced().key !== '' && _getAdvanced().secretKey !== '') {
+                        postObj.advanced = _getAdvanced();
+                    }
+                    $scope.widgetLog = [];
+
+                    _sendProlong();
+                    $.postMessage(JSON.stringify(postObj), $scope.postUrl, iframe.get(0).contentWindow);
                 };
 
-                $scope.stopWidget = function(){
+                $scope.stopWidget = function() {
                     $scope.play = false;
+                    var iframe = $element.find('#iframe');
+
+                    $.postMessage(JSON.stringify({name: 'stop_widget'}), $scope.postUrl, iframe.get(0).contentWindow);
                 };
 
+                $scope.isPreview = function() {
+                    return currentView === 'preview';
+                };
 
-                $scope.hideAdvanced = function(){
-                    return true;
+                $scope.credentialsChecked = function() {
+                    if ((currentView === 'registered' &&
+                        $scope.advanced.project_name.length > 0 &&
+                        $scope.advanced.hpcs_key.length > 0 &&
+                        $scope.advanced.hpcs_secret_key.length > 0) ||
+                        currentView !== 'registered' && !$scope.play) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+
+                $scope.advancedChange = function() {
+                    $scope.playEnabled = $scope.credentialsChecked() && $scope.currentStep === 4;
+                };
+
+                $scope.onTimeout = function() {
+                    milliseconds -= 1000;
+                    $scope.widgetTime = _millisecondsToTime(milliseconds);
+                    timeout = $timeout($scope.onTimeout, 1000);
+                };
+
+                $scope.$watch('selectedWidget', function(newWidget) {
+                    if (newWidget !== null) {
+                        $scope.play = false;
+                        $scope.playEnabled = true;
+                        $scope.widgetTime = '';
+                        $scope.manageUrl = null;
+                        $scope.consoleUrl = null;
+                        $scope.widgetLog = [];
+                        isNewWidgetSelected = true;
+                        _stopTimer();
+                    }
+                });
+
+                function _getAdvanced() {
+                    return {
+                        project: $scope.advanced.project_name,
+                        key: $scope.advanced.hpcs_key,
+                        secretKey: $scope.advanced.hpcs_secret_key
+                    };
                 }
-                $scope.playDisabled = function(){
-                    // this is where we implement advanced data is missing - disable play button feature.
-                    return false;
-                };
 
-//                $('#play_btn').live('click', function() {
-//                    if (!credentialsChecked()) {
-//                        return;
-//                    }
-//
-//                    var iframe = $('#iframe');
-//                    var postObj = {name: 'play_widget'};
-//                    if (getAdvanced().project !== '' && getAdvanced().key !== '' && getAdvanced().secretKey !== '') {
-//                        postObj.advanced = getAdvanced();
-//                    }
-//
-//                    updateButtonState('play');
-//
-//                    $('#iframe').trigger({type: "prolong"});
-//                    $.postMessage(JSON.stringify(postObj), postUrl, iframe.get(0).contentWindow);
-//                });
-//
-//                $('#stop_btn').live('click', function () {
-//                    var iframe = $('#iframe');
-//
-//                    updateButtonState('stop');
-//
-//                    $.postMessage(JSON.stringify({name: 'stop_widget'}), postUrl, iframe.get(0).contentWindow);
-//                });
-//
-//                $('input[name="project_name"], input[name="hpcs_key"], input[name="hpcs_secret_key"]').live('change', function () {
-//                    if (credentialsChecked()) {
-//                        $('#play_btn').removeClass('disabled');
-//                    } else {
-//                        $('#play_btn').addClass('disabled');
-//                    }
-//                });
-//
-//                $('#iframe').live('widget_change', function() {
-//                    updateButtonState('stop');
-//                });
-//
-//                function setPlayEventHandling() {
-//                    $('#play_btn').on('click', function() {});
-//                }
-//
-//                function getAdvanced() {
-//                    var $advanced = $('#advanced');
-//                    var $project = $advanced.find('[name=project_name]');
-//                    var $key = $advanced.find('[name=hpcs_key]');
-//                    var $secretKey = $advanced.find('[name=hpcs_secret_key]');
-//
-//                    return {project: $project.val(), key: $key.val(), secretKey: $secretKey.val()};
-//                }
-//
-//                function updateButtonState(state) {
-//                    if (state === 'play' && credentialsChecked()) {
-//                        $('#stop_btn').show();
-//                        $('#play_btn').hide();
-//                    } else if (state === 'stop') {
-//                        $('#stop_btn').hide();
-//                        $('#play_btn').show();
-//                    }
-//                }
-//
-//                function credentialsChecked() {
-//                    if ((currentView === 'registered' &&
-//                        $('input[name="project_name"]').val().length > 0 &&
-//                        $('input[name="hpcs_key"]').val().length > 0 &&
-//                        $('input[name="hpcs_secret_key"]').val().length > 0)
-//                        || currentView !== 'registered' && !$('#play_btn').hasClass('disabled')) {
-//                        return true;
-//                    } else {
-//                        return false;
-//                    }
-//                }
-//
-//                $.receiveMessage(function (e) {
-//                        try {
-//                            var msg = JSON.parse(e.data);
-//                            var $log = $('#log');
-//
-//                            if (msg.name === 'widget_log') {
-//                                $('#iframe').trigger({
-//                                    type: 'widget_log',
-//                                    html: msg.message,
-//                                    class: msg.type
-//                                });
-//                                $log.scrollTop($log[0].scrollHeight);
-//
-//                                if (msg.type == 'error') {
-//                                    $('#iframe').trigger({
-//                                        type: "error",
-//                                        status: msg.status
-//                                    });
-//                                }
-//                            } else if (msg.name === 'set_advanced') {
-//                                var $advanced = $('#advanced');
-//                                var $project = $advanced.find('[name=project_name]');
-//                                var $key = $advanced.find('[name=hpcs_key]');
-//                                var $secretKey = $advanced.find('[name=hpcs_secret_key]');
-//
-//                                $project.val(msg.project);
-//                                $key.val(msg.key);
-//                                $secretKey.val(msg.secretKey);
-//                            } else if (msg.name === 'widget_status') {
-//                                $('#iframe').trigger({
-//                                    type: "widget_status",
-//                                    status: msg.status
-//                                });
-//
-//                                updateButtonState(msg.status.state === 'STOPPED' ? 'stop' : 'play');
-//                            } else if (msg.name === 'stop_widget') {
-//                                $('#iframe').trigger({
-//                                    type: "stop_widget",
-//                                    status: msg.status
-//                                });
-//                            } else if (msg.name === 'play_widget') {
-//                                $('#iframe').trigger({
-//                                    type: 'play_widget',
-//                                    status: msg.status
-//                                });
-//                            }
-//                        } catch (exception) {
-//                            console.log(['problem invoking callback for ', e, exception]);
-//                        }
-//                    },
-//                    function () {
-//                        return true;
-//                    }
-//                );
-//
-//                updateButtonState('stop');
+                function _startTimer() {
+                    _stopTimer();
+                    timeout = $timeout($scope.onTimeout, 1000);
+                }
+
+                function _stopTimer() {
+                    $timeout.cancel(timeout);
+                }
+
+                function _millisecondsToTime(milli) {
+                    var seconds = Math.floor((milli / 1000) % 60);
+                    var minutes = Math.floor((milli / (60 * 1000)) % 60);
+                    var days = Math.floor(milli / (1000 * 60 * 60 * 24));
+                    var timeToDisplay = '';
+
+                    if (seconds < 0 || minutes < 0) {
+                        seconds = '--';
+                        minutes = '--';
+                    }
+
+                    if (days > 0) {
+                        timeToDisplay = days + ' Days';
+                    } else {
+                        timeToDisplay = minutes + ':' + (seconds < 10 ? '0' : '') + seconds + ' Min.';
+                    }
+
+                    return timeToDisplay;
+                }
+
+                function _sendProlong() {
+                    var data = {
+                        'leadId' : $cookieStore.get('leadId'),
+                        'instanceId' : $cookieStore.get('instanceId')
+                    };
+
+                    if (data.leadId !== undefined && data.instanceId !== undefined) {
+                        widgetService.prolong(data);
+                    }
+                }
+
+                function _scrollLog() {
+                    var log = $element.find('#log');
+                    log.scrollTop(log[0].scrollHeight);
+                }
+
+                $.receiveMessage(function (e) {
+                    var msg = JSON.parse(e.data);
+
+                    if (handlers.hasOwnProperty(msg.name)) {
+                        try {
+                            handlers[msg.name](e);
+                        } catch (exception) {
+                            console.log(['problem invoking callback for ', e, exception]);
+                        }
+                    }
+                });
             }
         };
     });
