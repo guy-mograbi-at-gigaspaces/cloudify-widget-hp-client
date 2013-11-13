fdda60677638315cced3283683a889a4c12b2a28
Erez Carmel <erez@gigaspaces.com>
2013-11-13 15:05:15 +0200

    CW-55

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('cloudifyWidgetHpClientApp')
-    .controller('MainCtrl', function ($scope, $location) {
+    .controller('MainCtrl', function ($scope, $location, widgetService) {
+
+        $scope.widgetsList = [];
+        $scope.conf = window.conf;
+
+        widgetService.getWidgetList()
+            .then(function(data) {
+                $scope.widgetsList = data;
+            });
 
         $scope.redirectPage = function(pageName) {
             $location.path('/' + pageName);
